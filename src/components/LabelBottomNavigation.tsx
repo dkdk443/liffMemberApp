27c6ff1856a40cf89d6014d320f74465575d83c8
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-12-05 19:15:05 +0900

    iPhoneのノッチ対策

--- src/components/LabelBottomNavigation.tsx
+++ src/components/LabelBottomNavigation.tsx
@@ -1,31 +1,34 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Icon from '@material-ui/core/Icon';
+import liff from '@line/liff/dist/lib';
+liff
+
+const Nav = styled.div`
+  width: 100vw;
+  background-image: linear-gradient(90deg, rgba(57, 210, 248, 1), rgba(48, 97, 96, 1));
+  color: #fff;
+  border-radius: 28px;
+`;
+
+const ButtonList = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+const ButtonItem = styled.div`
+  font-size: 12px;
+  height: 80px;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+  text-align: center;
+  align-items: center;
+`;
 
 const LabelBottomNavigation = () => {
-  const Nav = styled.div`
-    width: 100vw;
-    background-color: rgba(48, 97, 96, 1);
-    color: #fff;
-  `;
-
-  const ButtonList = styled.ul`
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-  `;
-
-  const ButtonItem = styled.div`
-    font-size: 10px;
-    height: 60px;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    padding: 8px;
-    text-align: center;
-    align-items: center;
-  `;
-
   type NavItem = {
     id: number;
     path: string;
@@ -60,8 +63,20 @@ const LabelBottomNavigation = () => {
     },
   ];
 
+// iPhoneX~をセーフエリアが必要だとする 
+  const isNeedSafeArea = navigator.userAgent.match(/iPhoneX/i) ? true : false;
+
+// Safari判定　　参考：https://tagsqa.com/detail/9178　
+  const isSafari = navigator.vendor &&
+    navigator.vendor.indexOf('Apple') > -1 &&
+    navigator.userAgent &&
+    navigator.userAgent.indexOf('CriOS') == -1 &&
+    navigator.userAgent.indexOf('FxiOS') == -1;
+  // liffブラウザ上で動作しているか
+  const isInLiffClient = liff.isInClient
+
   return (
-    <Nav>
+    <Nav style={isNeedSafeArea && (isSafari || isInLiffClient) ? { paddingBottom: 'calc(env(safe-area-inset-bottom) + 44px)'} : {paddingBottom: '0px'}}>
       <ButtonList>
         {navigations.map(nav => {
             return(
