5e3308f8e5325ab4df446fca2b164c7659243d46
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-12-15 10:10:15 +0900

    デプロイ用にコメントアウト

--- src/components/QRcode.tsx
+++ src/components/QRcode.tsx
@@ -20,7 +20,7 @@ const Card = styled.div`
 
 const QRcode = (props: any) => {
   let lineUserId = props.profile.userId;
-  const { Canvas } = useQRCode();
+  // const { Canvas } = useQRCode();
 
   if (!lineUserId) {
     return (
@@ -28,24 +28,24 @@ const QRcode = (props: any) => {
     )
   } else {
     return (
-      <Card>
-        <Canvas
-          text={lineUserId}
-          options={{
-            type: 'image/jpeg',
-            quality: 0.3,
-            level: 'M',
-            margin: 3,
-            scale: 4,
-            width: 240,
-            color: {
-              // dark: '#010599FF',
-              light: '#FFF',
-            },
-          }}
-        />
-        {/* <div>{ lineUserId }</div> */}
-      </Card>
+      // <Card>
+      //   <Canvas
+      //     text={lineUserId}
+      //     options={{
+      //       type: 'image/jpeg',
+      //       quality: 0.3,
+      //       level: 'M',
+      //       margin: 3,
+      //       scale: 4,
+      //       width: 240,
+      //       color: {
+      //         // dark: '#010599FF',
+      //         light: '#FFF',
+      //       },
+      //     }}
+      //   />
+      {/* <div>{ lineUserId }</div> */ }
+      // </Card>
     )
   }
 
