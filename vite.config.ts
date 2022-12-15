77deee27ed3981a8d7aed1da0fcb3aa21a6cd493
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-12-15 09:58:49 +0900

    viteのコンパイル用のプラグイン追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // @ts-ignore
+    viteCommonjs(['qrcode'])
+  ],
   server: {
     host: true
   }
